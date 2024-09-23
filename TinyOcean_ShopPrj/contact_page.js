6a539b7bdd713ae7aafc7bc7b0d0c54d1716ea5f
nhannm-vn <nhanmin911@gmail.com>
2024-09-23 23:57:47 +0700

    update cho ba tut met ghe

--- TinyOcean_ShopPrj/contact_page.js
+++ TinyOcean_ShopPrj/contact_page.js
@@ -167,4 +167,14 @@ document.querySelectorAll(".input-check").forEach((inputNode) => {
     });
 });
 
+//**Giúp cho trang web chuyển mượt hơn
+window.transitionToPage = function(href) {
+    document.querySelector('body').style.opacity = 0
+    setTimeout(function() { 
+        window.location.href = href
+    }, 1000)
+};
 
+document.addEventListener('DOMContentLoaded', function(event) {
+    document.querySelector('body').style.opacity = 1
+});
