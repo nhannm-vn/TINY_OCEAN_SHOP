56b921064317091deb3b7ea9b75508afd69bdf10
nhannm-vn <nhanmin911@gmail.com>
2024-10-03 22:33:01 +0700

    update 3-10-2024

---
+++ TinyOcean_ShopPrj/Home/page-home.js
@@ -0,0 +1,22 @@
+//**Giúp cho trang web chuyển mượt hơn
+window.transitionToPage = function(href) {
+    document.querySelector('body').style.opacity = 0
+    setTimeout(function() { 
+        window.location.href = href
+    }, 1000)
+};
+
+document.addEventListener('DOMContentLoaded', function(event) {
+    document.querySelector('body').style.opacity = 1
+});
+
+//-------------------------------------------------------------
+//**Giúp cho bấm vào nút nào thì nút đó sẽ có thêm class đặc biệt 
+//mà ai có class đó thì sẽ bị dính một màu
+//**Hàm giúp biết đang đứng ở trang nào thì nút trang đó sáng lên
+document.querySelectorAll(".link-pop-up").forEach((link) => {
+    if(link.id == "pop"){
+        link.classList.add("actived");
+        // link.parentElement.previousElementSibling.classList.add("actived");
+    };
+});
