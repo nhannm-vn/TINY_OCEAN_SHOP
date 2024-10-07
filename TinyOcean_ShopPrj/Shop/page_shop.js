a787df713c671bedee71f268ea58cdd53ce69686
nhannm-vn <nhanmin911@gmail.com>
2024-10-07 22:07:57 +0700

    update 7-10 ne

--- TinyOcean_ShopPrj/Shop/page_shop.js
+++ TinyOcean_ShopPrj/Shop/page_shop.js
@@ -1,3 +1,26 @@
+//**Giúp cho trang web chuyển mượt hơn
+window.transitionToPage = function(href) {
+    document.querySelector('body').style.opacity = 0
+    setTimeout(function() { 
+        window.location.href = href
+    }, 1000)
+};
+
+document.addEventListener('DOMContentLoaded', function(event) {
+    document.querySelector('body').style.opacity = 1
+});
+
+//-------------------------------------------------------------
+//**Hàm giúp biết đang đứng ở trang nào thì nút trang đó sáng lên
+document.querySelectorAll(".link-pop-up").forEach((link) => {
+    if(link.getAttribute("id") == "pop"){
+        link.classList.add("actived");
+    };
+});
+
+
+
+// chức năng tăng giảm khi mua sản phẩm
 document.addEventListener('DOMContentLoaded', function () {
     const quantityInputs = document.querySelectorAll('.quantity-input');
     const decreaseButtons = document.querySelectorAll('.btn-decrease');
